7f5a31c3953d26fb8f9daca12a909cfa2f6fadbc
lipollis <aline.pollis@hotmail.com>
2022-02-01 10:39:46 -0300

    Create components: navbar, painel, cadastrar, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { PainelVagasComponent } from './painel-vagas/painel-vagas.component';
+import { CadastrarVagasComponent } from './cadastrar-vagas/cadastrar-vagas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    PainelVagasComponent,
+    CadastrarVagasComponent
   ],
   imports: [
     BrowserModule,
